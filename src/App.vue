d9fba99292786b10fcd29e27439b4a939c60382c
ElpolMorris <pmorales.contacto@gmail.com>
2021-01-05 15:38:34 -0300

    CRD -U

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@
     <label>Nombre:</label> <input v-model="nombre" /> <br />
     <label>Hobbie:</label> <input v-model="hobbie" /> <br />
     <button @click="addUsers">Agregar</button>
-    <b-table striped hover :items='usuarios'></b-table>
+    <b-table striped hover :items='usuarios'>
+      <!-- <template #cell(acciones)="data"> -->
+      <template #cell(acciones)="pablo">
+        <b-button v-b-modal.modal-1 variant="warning" >Editar</b-button>
+        <b-button variant="danger" @click="eraseUser(pablo.index)" >Borrar</b-button>
+      </template>
+    </b-table>
+</div>
   </div>
 </template>
 
@@ -15,14 +22,19 @@ export default {
   name: "App",
   data() {
     return {
+      fields: ['nombre', 'hobbie','acciones'],
       nombre: "",
       hobbie: "",
       usuarios: [],
+      on: false
     };
   },
   methods: {
     addUsers() {
-      this.usuarios.push({ nombre: this.nombre, hobbie: this.hobbie });
+      this.usuarios.push({ nombre: this.nombre, hobbie: this.hobbie, acciones: ''});
+    },
+    eraseUser(indice){
+      this.usuarios.splice(indice,1)
     },
   },
   components: {
